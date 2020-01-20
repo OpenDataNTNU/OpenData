2bd2961700166cd895842fa486fe2d16cbab7fa2
Andreasjj <andreasjj@gmail.com>
2020-01-20 10:02:34 +0100

    Changed name of styled component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 import { GlobalStyle } from './global-styles';
 import { Router } from './router';
 
-const AppWrapper = styled.div`
+const Wrapper = styled.div`
   text-align: center;
 `
 
 function App() {
   return (
     <React.Fragment>
-      <AppWrapper>
+      <Wrapper>
         <Router />
-      </AppWrapper>
+      </Wrapper>
       <GlobalStyle />
     </React.Fragment>
   );
