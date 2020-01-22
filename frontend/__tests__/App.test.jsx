31d0315d7abd5bc6f3072442ff9533e8a0441f87
Andreasjj <andreasjj@gmail.com>
2020-01-22 16:33:11 +0100

    Added missing import

--- frontend/__tests__/App.test.jsx
+++ frontend/__tests__/App.test.jsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../src/App';
 import { Provider } from 'react-redux';
+import { store } from './state/store';
 
 test('renders learn react link', () => {
   const { getByText } = render(
