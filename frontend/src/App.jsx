bc4ce9dbba5ab3335ac5cec2887583fde57cdb97
Andreasjj <andreasjj@gmail.com>
2020-01-22 16:15:04 +0100

    set up Redux and added alerts (through redux)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { GlobalStyle } from './global-styles';
 import { Router } from './router';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { history } from './router';
+import { alertActions } from './state/actions/alert';
+import { Alert } from './sharedComponents/Alert';
 
 const Wrapper = styled.div`
   text-align: center;
 `
 
 function App() {
+  // Redux dispatch
+  const dispatch = useDispatch();
+
+  // Global state
+  const alert = useSelector(state => state.alert)
+
+  useEffect(() => {
+		// clear alert on location change
+    history.listen((location, action) => {
+        removeAlert();
+    });
+  }, [])
+
+  // callback to remove alert
+  const removeAlert = () => {
+    dispatch(alertActions.clear());
+  }
+
   return (
     <React.Fragment>
       <Wrapper>
+        {
+          alert.type 
+          ? 
+            <Alert alert={alert} removeToast={removeAlert} /> 
+          : 
+            null
+        }
         <Router />
       </Wrapper>
       <GlobalStyle />
---
+++ frontend/src/state/constants/alert.js
@@ -0,0 +1,10 @@
+const alertConstants = {
+  SUCCESS: 'ALERT_SUCCESS',
+  INFO: 'ALERT_INFO,',
+  ERROR: 'ALERT_ERROR',
+  CLEAR: 'ALERT_CLEAR',
+};
+
+export {
+  alertConstants,
+};
