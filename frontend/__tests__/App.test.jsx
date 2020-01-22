752514ce126d680628ffcff924e67642a25bdd1f
Andreasjj <andreasjj@gmail.com>
2020-01-22 16:32:03 +0100

    Updated test and added missing semicolons

--- frontend/__tests__/App.test.jsx
+++ frontend/__tests__/App.test.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../src/App';
+import { Provider } from 'react-redux';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
