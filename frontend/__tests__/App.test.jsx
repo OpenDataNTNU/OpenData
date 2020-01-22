a04f26c7b3789207e5b28ce3a9b88cabae1e42f9
Andreasjj <andreasjj@gmail.com>
2020-01-22 17:47:31 +0100

    Added tests

--- frontend/__tests__/App.test.jsx
+++ frontend/__tests__/App.test.jsx
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import App from '../src/App';
 import { Provider } from 'react-redux';
 import { store } from '../src/state/store';
+import { alertActions } from '../src/state/actions/alert';
 
 test('renders learn react link', () => {
   const { getByText } = render(
@@ -14,3 +15,4 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
