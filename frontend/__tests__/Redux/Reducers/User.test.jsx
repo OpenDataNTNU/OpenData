551d930b5168a3696a17ddd7528eaef18d57e35e
Andreasjj <andreasjj@gmail.com>
2020-01-23 14:19:27 +0100

    Fixed user reducer and added tests

--- frontend/__tests__/Redux/Reducers/User.test.jsx
+++ frontend/__tests__/Redux/Reducers/User.test.jsx
@@ -1,6 +1,6 @@
 import { user as userReducer} from '../../../src/state/reducers/user';
 
-describe('alert reducer', () => {
+describe('user reducer', () => {
   it('should return the initial state', () => {
       expect(userReducer(undefined, {})).toEqual({});
   });
