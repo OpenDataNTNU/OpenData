d8dddfb9e654c9d62cc3396a04b4cd455f5be53d
Andreasjj <andreasjj@gmail.com>
2020-01-23 17:35:11 +0100

    Added login and register components

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,103 @@
+import React, {useState, useEffect} from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+
+//import { userActions } from '../../state/actions/user';
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  justify-items: center;
+  background-color: #f5f5f5;
+  height: 100%;
+  width: 100%;
+`
+
+const Form = styled.form`
+  max-width: 30em;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  background-color: white;
+  border-radius: 0.3em;
+  -webkit-box-shadow: 0 0.0625em 0.125em rgba(0,0,0,0.15);
+  -moz-box-shadow: 0 0.0625em 0.125em rgba(0,0,0,0.15);
+  box-shadow: 0 0.0625em 0.125em rgba(0,0,0,0.15);
+`
+
+const Input = styled.input`
+  padding: 0.3em;
+  font-size:1.1em;
+  border-radius: 0.3em;
+  border: solid 0.1em lightgrey;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0.3em 0 0.3em 0;
+`
+
+const Button = styled.button`
+  padding: 0.3em;
+  font-size:1.1em;
+  border-radius: 0.3em;
+  color: #7e6dad;
+  background-color: #dcd8ff;
+  border: solid 0.1em #9a85d3;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 1em 0 0.8em 0;
+  cursor: pointer
+`
+
+const A = styled.a`
+  color: #434faf;
+  text-decoration: underline;
+`
+
+const Login = (props) => {
+  // State
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  // Redux
+  const dispatch = useDispatch()
+
+  const onChange = (e) => {
+    switch(e.target.name) {
+      case "email": {
+        setEmail(e.target.value)
+        break;
+      }
+      case "password": {
+        setPassword(e.target.value)
+        break;
+      }
+    }
+  }
+
+  const login = (e) => {
+    e.preventDefault()
+
+    //dispatch(userActions.login(email, password))
+  }
+
+  return (
+    <Wrapper>
+        <Form onSubmit={login}>
+          <h1>Login</h1>
+          <Input type="text" placeholder="Email" name="email" value={email} onChange={onChange} />
+          <Input type="password" placeholder="Password" name="password" value={password} onChange={onChange}  />
+          <Button type="submit">Sign in</Button>
+          <p>Not a user yet? <A href="Signup.html">Sign up here</A></p>
+        </Form>
+    </Wrapper>
+  );
+}
+
+export {
+  Login,
+};
