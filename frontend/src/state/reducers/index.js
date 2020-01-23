a10a6e097fa563801e4fe1e474566dfbe9beb425
Andreasjj <andreasjj@gmail.com>
2020-01-23 13:42:15 +0100

    Added user reducer to root reducer

--- frontend/src/state/reducers/index.js
+++ frontend/src/state/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { alert } from './alert';
+import { user } from './user';
 
 const rootReducer = combineReducers({
   alert,
+  user,
 });
 
 export {
