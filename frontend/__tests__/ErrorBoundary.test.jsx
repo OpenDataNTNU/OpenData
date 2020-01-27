b28a737a4fa3117add2fee5b8c2ca7a8ff3b6517
Andreasjj <andreasjj@gmail.com>
2020-01-27 16:12:10 +0100

    Added missing changes

--- frontend/__tests__/ErrorBoundary.test.jsx
+++ frontend/__tests__/ErrorBoundary.test.jsx
@@ -6,17 +6,17 @@ import { ErrorBoundary } from '../src/ErrorBoundary';
 
 test('Error boundary is rendered on error', () => {
   // Test component that will throw an error
-  const TEST = (props) => {
+  const TEST = () => {
     useEffect(() => {
-      throw new Error("hi")
-    }, [])
+      throw new Error('hi');
+    }, []);
     return (
       <h1>TEST</h1>
-    )
-  }
+    );
+  };
 
   // Render error boundary and test component
-  const { container, getByText } = render(
+  const { getByText } = render(
     <ErrorBoundary>
       <TEST />
     </ErrorBoundary>,
