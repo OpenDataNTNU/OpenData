1a3228c357234caae7e0a7e1e1ba9bf50c8d819e
Alexander Andersen <mittbrukernavn.aa@gmail.com>
2020-02-10 16:25:18 +0100

    Add api calls to MetadataByType

--- frontend/src/pages/MetadataByType/MetadataByTypeBody.jsx
+++ frontend/src/pages/MetadataByType/MetadataByTypeBody.jsx
@@ -1,3 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-export const MetadataByTypeBody = () => <div />;
+export const MetadataByTypeBody = (props) => {
+  const { name } = props;
+  const [metadatas, setMetadatas] = useState([]);
+
+  useEffect(() => {
+    const internal = async () => {
+      const res = await fetch(`/api/metadataType/${name}`);
+      const { metadataList } = await res.json();
+      setMetadatas(metadataList);
+    };
+    internal();
+  }, [name]);
+  return (
+    <div>
+      <h1>
+        Metadata type:
+        {` ${name}`}
+      </h1>
+      {metadatas.map(({ uuid, municipalityName }) => (
+        <a key={uuid} href={`/viewData/dataset/${uuid}`}>
+          <p>
+            {municipalityName}
+          </p>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+MetadataByTypeBody.propTypes = {
+  name: PropTypes.string.isRequired,
+};
