c310739d13dee9bd88c7b09c58fb9ac174e5a2c2
Alexander Andersen <mittbrukernavn.aa@gmail.com>
2020-02-12 16:10:37 +0100

    Commented out future functionality

--- frontend/src/pages/Inspection/ViewMetadataBody.jsx
+++ frontend/src/pages/Inspection/ViewMetadataBody.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import { MetaData } from './MetaData';
-import { Comments } from './Comments';
+// import { Comments } from './Comments';
 
 const Wrapper = styled.div`
   flex: 1;
@@ -22,7 +22,7 @@ export const ViewMetadataBody = (props) => {
     metadataTypeName: '',
     municipalityName: '',
   });
-  const [comments, setComments] = useState([]);
+  // const [comments, setComments] = useState([]);
   const [tags, setTags] = useState([]);
   const [description, setDescription] = useState('');
 
@@ -32,6 +32,7 @@ export const ViewMetadataBody = (props) => {
       const municipality = await res.json();
       setData(municipality);
       // get received comments when this is implemented backend
+      /*
       setComments([{
         id: 1,
         comment: 'Beautiful comment',
@@ -44,6 +45,7 @@ export const ViewMetadataBody = (props) => {
         author: 'Sharknado',
         date: '23-07-2019',
       }]);
+      */
     };
     internal();
   }, [id]);
@@ -65,7 +67,7 @@ export const ViewMetadataBody = (props) => {
   return (
     <Wrapper>
       <MetaData data={data} description={description} tags={tags} />
-      <Comments comments={comments} />
+      {/* <Comments comments={comments} /> */}
     </Wrapper>
   );
 };
