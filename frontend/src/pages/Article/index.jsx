1a52ce4e3c08773ac493db69c838d943f808a475
Andreas <andreasjj@gmail.com>
2020-02-21 23:26:28 +0100

    Story/6 (#97)
    * Feature/view experience article (#93)
    
    * Added base components and new module
    
    * Added componends
    
    * Added base components and new module
    
    * Added componends
    
    * Delete XUnitTest_backend.AssemblyInfo.cs
    
    * Delete XUnitTest_backend.AssemblyInfoInputs.cache
    
    * Delete project.razor.json
    
    * Delete yarn.lock
    
    * Changed code to use api
    
    * Linter fixes and stuff
    
    * Removed unused dependency
    
    * Updated lock
    
    * Fixed stuff
    
    * Delete XUnitTest_backend.csprojAssemblyReference.cache

---
+++ frontend/src/pages/Article/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import parse from 'html-react-parser';
+
+import { Template } from '../../sharedComponents/Template';
+import { useGetExperienceArticle } from '../../sharedComponents/hooks';
+
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 960px;
+  margin: auto;
+  box-sizing: border-box;
+  @media screen and (max-width: 960px) {
+    padding: 5px;
+  }
+`;
+
+const Title = styled.h2`
+  margin-bottom: 5px;
+`;
+
+const Tags = styled.p`
+  display: flex;
+  opacity: 60%;
+  margin: 5px 0px 10px 0px;
+`;
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 400px;
+  flex: 1;
+  margin-bottom: 10px;
+  padding: 15px 0px;
+  border-top: 1px solid black;
+
+  & img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+`;
+
+const Article = () => {
+  // React router dom get id from history
+  const { id } = useParams();
+  const article = useGetExperienceArticle(id) || {};
+
+  return (
+    <Template>
+      <Wrapper>
+        <Title>{article.title || ''}</Title>
+        <Tags>
+          {
+            article.tags && article.tags.map((tag, index) => {
+              if (index + 1 === article.tags.length) {
+                return `${tag.tagName}`;
+              }
+              return `${tag.tagName}, `;
+            })
+          }
+        </Tags>
+        <Body>
+          {
+            parse(article.contents || '')
+          }
+        </Body>
+      </Wrapper>
+    </Template>
+  );
+};
+
+export {
+  Article,
+};
