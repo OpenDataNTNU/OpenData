72ace1d7f2fcf02ae2a9c935397167b99e9b76b5
mathiabr <mathiabr@stud.ntnu.no>
2020-02-24 14:42:47 +0100

    Added comment to Models.

---
+++ backend/Domain/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace OpenData.Domain.Models
+{
+    public class Comment
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Uuid { get; set; }
+
+        [ForeignKey("User")]
+        public string UserMail { get; set; }
+        public User User { get; set; }
+
+        public string Content { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Published { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Edited { get; set; }
+    }
+}
