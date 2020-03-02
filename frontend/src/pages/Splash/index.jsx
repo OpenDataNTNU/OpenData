d7cd1ddc6bc4e36b0d2b9ca670387d1e322911eb
larseva <larseva@stud.ntnu.no>
2020-03-02 17:46:00 +0100

    removed unused imports

--- frontend/src/pages/Splash/index.jsx
+++ frontend/src/pages/Splash/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-
 import { Template } from '../../sharedComponents/Template';
-import logo from '../../assets/logo.svg';
 import { AllDataBody } from '../allData/AllDataBody';
 
 const Splash = () => { // eslint-disable-line arrow-body-style
