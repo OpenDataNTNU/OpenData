e6736d94e8fe6500f9ee3a2e5b512752f4074a3a
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-03-09 20:40:19 +0100

    Add municipalities to the search page

--- frontend/src/pages/Search/SearchBody.jsx
+++ frontend/src/pages/Search/SearchBody.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
+import Multiselect from 'react-widgets/lib/Multiselect';
+import 'react-widgets/dist/css/react-widgets.css';
 
 import { SingleMetaDataResult } from '../../sharedComponents/Metadata/SingleMetaDataResult';
+import { useGetTags } from '../../sharedComponents/hooks/GetTags';
+import { useGetValidMunicipalities } from '../../sharedComponents/hooks/GetValidMunicipalities';
 import { alertActions } from '../../state/actions/alert';
 
 const Wrapper = styled.div`
@@ -15,16 +19,51 @@ const HorizontalWrapper = styled.div`
   flex-direction: row;
 `;
 
+const Select = styled(Multiselect)`
+  & > .rw-widget-picker > div {
+    display: flex;
+    align-items: center;
+    & > ul > li {
+      margin-top 0px;
+    }
+  }
+`;
+
 export const SearchBody = () => {
   const searchField = useRef('');
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState({
+    description: '',
+    tags: [],
+    municipalities: [],
+  });
+  const [tags, setTags] = useState([]);
+  const [municipalities, setMunicipalities] = useState([]);
   const [metadatas, setMetadatas] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
+  const allTags = useGetTags() || [];
+  const allMunicipalities = useGetValidMunicipalities() || [];
+  const allMunicipalityNames = allMunicipalities.map(({ name }) => name);
+
+
+  const updateQueries = () => {
+    setQuery({
+      description: searchField.current.value,
+      tags,
+      municipalities,
+    });
+  };
 
   useEffect(() => {
     const internal = async () => {
+      // TODO: do this filtering backend rather than frontend
+      // TODO: add tags into filtering
+      const {
+        description: queryDescription,
+        // tags: queryTags, - currently not used since tags are not loaded in this
+        municipalities: queryMunicipalities,
+      } = query;
       try {
         setLoading(true);
         const res = await fetch('/api/Metadata');
@@ -36,7 +75,14 @@ export const SearchBody = () => {
         }
         const receivedMetadatas = await res.json();
         const filteredMetadatas = receivedMetadatas
-          .filter(({ description }) => description.toLowerCase().includes(query.toLowerCase()));
+          // the description should contain the query
+          .filter(({ description }) => (
+            description.toLowerCase().includes(queryDescription.toLowerCase())
+          ))
+          // the municipality should be listed in the query, if any are listed
+          .filter(({ municipalityName }) => (
+            queryMunicipalities.length === 0 || queryMunicipalities.indexOf(municipalityName) >= 0
+          ));
         setMetadatas(filteredMetadatas);
         setLoading(false);
       } catch (err) {
@@ -50,7 +96,16 @@ export const SearchBody = () => {
     <Wrapper>
       <HorizontalWrapper>
         <input type="text" placeholder="Search" ref={searchField} />
-        <button type="button" onClick={() => setQuery(searchField.current.value)}>Search</button>
+        <Select data={allTags} value={tags} onChange={setTags} placeholder="Tags" />
+        <Select
+          data={allMunicipalityNames}
+          value={municipalities}
+          onChange={setMunicipalities}
+          placeholder="Municipalities"
+        />
+        <button type="button" onClick={updateQueries}>
+          Search
+        </button>
       </HorizontalWrapper>
       {loading ? (
         <h2>Loading...</h2>
