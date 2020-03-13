6d77497ea8e035f3ccfc7790d100f7a86763d539
Filip Jacobsen <fijaco98@gmail.com>
2020-03-13 13:03:32 +0100

    test httpGET success for metadata /-type

---
+++ backend/backendTests/MetadataTypeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace OpenData.backend
+{
+    public class MetadataTypeControllerTests
+    : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public MetadataTypeControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/metadatatype")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateDefaultClient();
+
+            // Act
+            var getDefaultResponse = await client.GetAsync(url);
+
+            // Assert
+            getDefaultResponse.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                getDefaultResponse.Content.Headers.ContentType.ToString());
+        }
+
+    }
+}
