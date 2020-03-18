d90311691d510c4db796880f46bd1fca08736e11
Elias <elias.vagan@gmail.com>
2020-03-18 11:59:56 +0100

    First iteration of the revised "My data" page. (#202)
    * First iteration of the revised "My data" page.
    
    * Update frontend/src/pages/MyData/MyDataStarred.jsx
    
    Smart solution
    
    Co-Authored-By: MittBrukernavn <mittbrukernavn.aa@gmail.com>
    
    * Removed unnecessary try-catch statement
    
    Co-authored-by: MittBrukernavn <mittbrukernavn.aa@gmail.com>

--- frontend/src/pages/MyData/index.jsx
+++ frontend/src/pages/MyData/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import { Template } from '../../sharedComponents/Template';
 import { MyDataBody } from './MyDataBody';
 
+const Background = styled.div`
+  width: 100%;
+  flex: 1;
+  background-color: #eeeeee;
+  padding: 0.5rem;
+  box-sizing: border-box;
+`;
+
 export const MyData = () => (
   <Template>
-    <MyDataBody />
+    <Background>
+      <MyDataBody />
+    </Background>
   </Template>
 );
