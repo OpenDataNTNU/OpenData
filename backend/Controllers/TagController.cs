385963fbdc311c4ebdff5a52c6b4dbdbf3524ba2
Mathias Brekkan <31246008+Letaguator@users.noreply.github.com>
2020-03-18 12:02:37 +0100

    Added auth to TagController (#196)

--- backend/Controllers/TagController.cs
+++ backend/Controllers/TagController.cs
@@ -12,27 +12,41 @@ using OpenData.Extensions;
 
 using System;
 using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 
 namespace OpenData.Controllers
 {
+    [Authorize]
 	[Route("/api/[controller]")]
 	public class TagController : Controller
 	{
 		private readonly ITagService _tagService;
 		private readonly IMapper _mapper;
 		private readonly IUnitOfWork _unitOfWork;
+		private readonly IHttpContextAccessor httpContextRetriever;
+		private readonly IUserService userService;
 
-		public TagController(ITagService tagService, IMapper mapper, IUnitOfWork unitOfWork) 
+		public TagController(
+            ITagService tagService,
+            IMapper mapper,
+            IUnitOfWork unitOfWork,
+            IHttpContextAccessor httpContextRetriever,
+		    IUserService userService
+            ) 
 		{
 			_tagService = tagService;
 			_mapper = mapper;
 			_unitOfWork = unitOfWork;
+			this.httpContextRetriever = httpContextRetriever;
+			this.userService = userService;
 		}
 
 		/// <summary>
 		/// Returns all known tags in the system.
 		/// </summary> 
-        /// <returns>All tags</returns>
+    /// <returns>All tags</returns>
+    [AllowAnonymous]
 		[HttpGet]
 		public async Task<IEnumerable<Tag>> GetAllAsync()
 		{
@@ -50,6 +64,12 @@ namespace OpenData.Controllers
 		{
 			if (!ModelState.IsValid)
 				return BadRequest(ModelState.GetErrorMessages());
+
+			var username = httpContextRetriever.HttpContext.User.Identity.Name;
+			var user = await userService.GetUserByMailAsync(username);
+
+			if (user.UserType < UserType.Municipality)
+				return BadRequest("Invalid user permissions, must have municipality privileges or higher.");
 
 			var result = await _tagService.SaveAsync(tag);
 
