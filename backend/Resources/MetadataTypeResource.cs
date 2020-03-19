7b5ba003f5971b8372991c64cd06f4f8ef2f41f7
Liam Svanåsbakken Crouch <spam@petterroea.com>
2020-03-19 10:14:35 +0100

    Fix some resources that werent updated in the MetadataType refactor (#206)

--- backend/Resources/MetadataTypeResource.cs
+++ backend/Resources/MetadataTypeResource.cs
@@ -5,6 +5,8 @@ namespace OpenData.Domain.Models
 {
 	public class MetadataTypeResource
 	{
+		public Guid Uuid { get; set; }
+		
 		public string Name { get; set; }
 		public IList<MetadataTypeTagMappingResource> Tags { get; set; }
 		public string Description { get; set; }
