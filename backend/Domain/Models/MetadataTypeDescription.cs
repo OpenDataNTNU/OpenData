f8c60c356d2529da2f7dae52998ac82bd923c80b
mathiabr <mathiabr@stud.ntnu.no>
2020-03-23 13:57:21 +0100

    Added models

---
+++ backend/Domain/Models/MetadataTypeDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenData.Domain.Models
+{
+    public class MetadataTypeDescription
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Uuid { get; set; }
+        public string Content { get; set; }
+
+        [ForeignKey("MetadataType")]
+        public Guid MetadataTypeUuid { get; set; }
+        public MetadataType MetadataType { get; set; }
+    }
+}
---
+++ backend/Domain/Models/MetadataTypeDescriptionVote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenData.Domain.Models
+{
+    public class MetadataTypeDescriptionVote
+    {
+        [ForeignKey("MetadataTypeDescription")]
+        public Guid MetadataTypeDescriptionUuid { get; set; }
+        public MetadataTypeDescription MetadataTypeDescription { get; set; }
+
+        [ForeignKey("User")]
+        public Guid UserUuid { get; set; }
+        public User User { get; set; }
+    }
+}
