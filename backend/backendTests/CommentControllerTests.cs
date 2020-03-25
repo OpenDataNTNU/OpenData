429c59d663ee93efc852332403e2f1f530956d8f
Filip Jacobsen <fijaco98@gmail.com>
2020-03-25 18:51:20 +0100

    Test new municipality user

--- backend/backendTests/CommentControllerTests.cs
+++ backend/backendTests/CommentControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using OpenData.Domain.Models;
+using OpenData.Resources;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace OpenData.backend
@@ -15,13 +20,13 @@ namespace OpenData.backend
         }
 
         /// <summary>
-        /// Asserts correct status code for all HttpGet calls on the comment controller
+        /// Asserts correct status code and correct content type for all HttpGet calls on the comment controller
         /// </summary>
         [Theory]
         [InlineData("/api/comment/metadata/{metadataUuid}")]
         [InlineData("/api/comment/experiencepost/{experiencePostUuid}")]
         [InlineData("/api/comment/childcomments/{commentUuid}")]
-        public async Task Get_EndpointsReturnSuccess(string url)
+        public async Task Get_EndpointsReturnSuccess_CorrectContentType(string url)
         {
             // Arrange
 
@@ -34,30 +39,45 @@ namespace OpenData.backend
             // Assert
 
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-        }
-
-        /// <summary>
-        /// Asserts correct content type for all HttpGet calls on the comment controller
-        /// </summary>
-        [Theory]
-        [InlineData("/api/comment/metadata/{metadataUuid}")]
-        [InlineData("/api/comment/experiencepost/{experiencePostUuid}")]
-        [InlineData("/api/comment/childcomments/{commentUuid}")]
-        public async Task Get_CorrectContentType(string url)
-        {
-            // Arrange
-
-            var client = _factory.CreateDefaultClient();
-
-            // Act
-
-            var response = await client.GetAsync(url);
-
-            // Assert
-
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
+
+        /// <summary>
+        /// Asserts correct status code and correct content type for all HttpPut calls on the comment controller
+        /// </summary>
+        //[Theory]
+        //[InlineData("/api/comment/metadata/{metadataUuid}")]
+        //public async Task Put_EndpointsReturnSuccess_CorrectContentType(string url)
+        //{
+        //    // Arrange
+
+        //    var client = _factory.CreateDefaultClient();
+        //    NewUserResource newUserResource = new NewUserResource();
+        //    newUserResource.Mail = "Put@test.no";
+        //    newUserResource.Password = "Put@passw0rd";
+        //    newUserResource.UserType = UserType.Standard;
+        //    NewCommentResource resource = new NewCommentResource();
+
+        //    // Act
+
+        //    await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
+        //    AuthUserResource loginResource = new AuthUserResource();
+        //    loginResource.Mail = newUserResource.Mail;
+        //    loginResource.Password = newUserResource.Password;
+        //    var loginResponse = await client.PostAsync("/api/user/auth", new StringContent(JsonSerializer.Serialize(loginResource), Encoding.UTF8, "application/json"));
+        //    Assert.NotNull(loginResponse.Content);
+        //    var user = ExtractResponse.Extract<PrivateSafeUserResource>(loginResponse);
+        //    Assert.Equal(loginResource.Mail, user.Mail);
+        //    var response = await client.PutAsync(url, new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
+
+        //    // Assert
+
+        //    response.EnsureSuccessStatusCode(); // Status Code 200-299
+        //    Assert.Equal("application/json; charset=utf-8",
+        //        response.Content.Headers.ContentType.ToString());
+        //}
+
 
     }
 }
--- backend/backendTests/UserControllerTests.cs
+++ backend/backendTests/UserControllerTests.cs
@@ -22,7 +22,7 @@ namespace OpenData.backend
         }
 
         /// <summary>
-        /// Asserts unauthorized results for all HttpGet calls on the user controller
+        /// Asserts unauthorized results for all non-authorized HttpGet calls on the user controller
         /// </summary>
         [Theory]
         [InlineData("/api/user")]
@@ -44,11 +44,11 @@ namespace OpenData.backend
         }
 
         /// <summary>
-        /// Asserts correct status code for all HttpPut calls on the user controller
+        /// Asserts user type to be equal to a municipality user, correct status code and correct content type for all HttpPut calls on the user controller
         /// </summary>
         [Theory]
         [InlineData("/api/user")]
-        public async Task Put_TestNewMunicipalityUser_EndpointsReturnSuccess(string url)
+        public async Task Put_NewMunicipalityUser_EndpointsReturnSuccess_CorrectContentType(string url)
         {
             // Arrange
 
@@ -61,15 +61,23 @@ namespace OpenData.backend
             // Act
 
             var response = await client.PutAsync(url, new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
+            AuthUserResource loginResource = new AuthUserResource();
+            loginResource.Mail = resource.Mail;
+            loginResource.Password = resource.Password;
+            var loginResponse = await client.PostAsync("/api/user/auth", new StringContent(JsonSerializer.Serialize(loginResource), Encoding.UTF8, "application/json"));
+            Assert.NotNull(loginResponse.Content);
+            var user = ExtractResponse.Extract<PrivateSafeUserResource>(loginResponse);
+            Assert.Equal(loginResource.Mail, user.Mail);
 
             // Assert
 
+            Assert.NotEqual(UserType.Standard, user.UserType);
+            Assert.NotEqual(UserType.Admin, user.UserType);
+            Assert.Equal(UserType.Municipality, user.UserType);
+            Assert.Equal("Test", user.MunicipalityName);
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-            //var user = JsonSerializer.Deserialize<PrivateSafeUserResource>(await response.Content.ReadAsStringAsync());
-            //SafeUserResource safeUserResource = new SafeUserResource();
-            //Assert.Equal(UserType.Municipality, safeUserResource.UserType);
-            //Assert.Equal("Bodø", user.MunicipalityName);
-            // TODO: FIks kommune stuff
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
         }
 
 
@@ -86,23 +94,23 @@ namespace OpenData.backend
             NewUserResource newUserResource = new NewUserResource();
             newUserResource.Mail = "Post@test.kommune.no";
             newUserResource.Password = "Post@passw0rd";
-            await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
-            AuthUserResource resource = new AuthUserResource();
-            resource.Mail = "Post@test.kommune.no";
-            resource.Password = "Post@passw0rd";
 
             // Act
 
+            await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
+            AuthUserResource resource = new AuthUserResource();
+            resource.Mail = newUserResource.Mail;
+            resource.Password = newUserResource.Password;
             var response = await client.PostAsync(url, new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
-            
+            Assert.NotNull(response.Content);
+            var user = ExtractResponse.Extract<PrivateSafeUserResource>(response);
+            Assert.Equal(resource.Mail, user.Mail);
+
             // Assert
 
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
-            Assert.NotNull(response.Content);
-            var user = ExtractResponse.Extract<PrivateSafeUserResource>(response);
-            Assert.Equal(resource.Mail, user.Mail);
         }
 
         /// <summary>
@@ -119,19 +127,19 @@ namespace OpenData.backend
             NewUserResource newUserResource = new NewUserResource();
             newUserResource.Mail = "Get@test.kommune.no";
             newUserResource.Password = "Get@passw0rd";
+
+            // Act
+
             await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
             AuthUserResource loginResource = new AuthUserResource();
-            loginResource.Mail = "Get@test.kommune.no";
-            loginResource.Password = "Get@passw0rd";
+            loginResource.Mail = newUserResource.Mail;
+            loginResource.Password = newUserResource.Password;
             var loginResponse = await client.PostAsync("/api/user/auth", new StringContent(JsonSerializer.Serialize(loginResource), Encoding.UTF8, "application/json"));
             Assert.NotNull(loginResponse.Content);
             var user = ExtractResponse.Extract<PrivateSafeUserResource>(loginResponse);
             Assert.Equal(loginResource.Mail, user.Mail);
             string token = user.Token;
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            // Act
-
             var response = await client.GetAsync(url);
 
             // Assert
