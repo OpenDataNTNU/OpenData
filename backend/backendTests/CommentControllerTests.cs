94137fa0484c146a99dc1a09a30025cc1e4b4695
Filip Jacobsen <fijaco98@gmail.com>
2020-03-26 14:46:34 +0100

    bort! bort, få det bort!

--- backend/backendTests/CommentControllerTests.cs
+++ backend/backendTests/CommentControllerTests.cs
@@ -43,41 +43,5 @@ namespace OpenData.backend
                 response.Content.Headers.ContentType.ToString());
         }
 
-        /// <summary>
-        /// Asserts correct status code and correct content type for all HttpPut calls on the comment controller
-        /// </summary>
-        //[Theory]
-        //[InlineData("/api/comment/metadata/{metadataUuid}")]
-        //public async Task Put_EndpointsReturnSuccess_CorrectContentType(string url)
-        //{
-        //    // Arrange
-
-        //    var client = _factory.CreateDefaultClient();
-        //    NewUserResource newUserResource = new NewUserResource();
-        //    newUserResource.Mail = "Put@test.no";
-        //    newUserResource.Password = "Put@passw0rd";
-        //    newUserResource.UserType = UserType.Standard;
-        //    NewCommentResource resource = new NewCommentResource();
-
-        //    // Act
-
-        //    await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
-        //    AuthUserResource loginResource = new AuthUserResource();
-        //    loginResource.Mail = newUserResource.Mail;
-        //    loginResource.Password = newUserResource.Password;
-        //    var loginResponse = await client.PostAsync("/api/user/auth", new StringContent(JsonSerializer.Serialize(loginResource), Encoding.UTF8, "application/json"));
-        //    Assert.NotNull(loginResponse.Content);
-        //    var user = ExtractResponse.Extract<PrivateSafeUserResource>(loginResponse);
-        //    Assert.Equal(loginResource.Mail, user.Mail);
-        //    var response = await client.PutAsync(url, new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
-
-        //    // Assert
-
-        //    response.EnsureSuccessStatusCode(); // Status Code 200-299
-        //    Assert.Equal("application/json; charset=utf-8",
-        //        response.Content.Headers.ContentType.ToString());
-        //}
-
-
     }
 }
--- backend/backendTests/MetadataControllerTests.cs
+++ backend/backendTests/MetadataControllerTests.cs
@@ -61,46 +61,5 @@ namespace OpenData.backend
                 response.Content.Headers.ContentType.ToString());
         }
 
-        //[Theory]
-        //[InlineData("/api/metadata")]
-        //public async Task Put_EndpointsReturnSuccess_CorrectContentType(string url)
-        //{
-        //    // Arrange
-
-        //    var client = _factory.CreateDefaultClient();
-        //    NewUserResource newUserResource = new NewUserResource();
-        //    newUserResource.Mail = "PutMetadata@test.kommune.no";
-        //    newUserResource.Password = "PutMetadata@passw0rd";
-        //    newUserResource.UserType = UserType.Municipality;
-        //    await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
-        //    AuthUserResource loginResource = new AuthUserResource();
-        //    loginResource.Mail = newUserResource.Mail;
-        //    loginResource.Password = newUserResource.Password;
-        //    var loginResponse = await client.PostAsync("/api/user/auth", new StringContent(JsonSerializer.Serialize(loginResource), Encoding.UTF8, "application/json"));
-        //    Assert.NotNull(loginResponse.Content);
-        //    var user = ExtractResponse.Extract<PrivateSafeUserResource>(loginResponse);
-        //    Assert.Equal(loginResource.Mail, user.Mail);
-        //    string token = user.Token;
-        //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-        //    await client.GetAsync(url);
-        //    SaveMetadataResource resource = new SaveMetadataResource();
-        //    resource.Description = "";
-        //    resource.FormatName = "";
-        //    resource.MetadataTypeName = "";
-        //    resource.MunicipalityName = "";
-        //    resource.ReleaseState = EReleaseState.Red;
-        //    resource.Url = "";
-
-        //    // Act
-
-        //    var response = await client.PutAsync(url, new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
-
-        //    // Assert
-
-        //    response.EnsureSuccessStatusCode(); // Status Code 200-299
-        //    Assert.Equal("application/json; charset=utf-8",
-        //        response.Content.Headers.ContentType.ToString());
-        //}
-
     }
 }
