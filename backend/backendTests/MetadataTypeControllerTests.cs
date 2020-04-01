2c03d0f5847b2f78181e903f5c214e2429878aa1
Filip Jacobsen <fijaco98@gmail.com>
2020-04-01 16:39:22 +0200

    test the MetadataType controller

--- backend/backendTests/MetadataTypeControllerTests.cs
+++ backend/backendTests/MetadataTypeControllerTests.cs
@@ -1,4 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using OpenData.Domain.Models;
+using OpenData.Resources;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace OpenData.backend
@@ -15,11 +21,62 @@ namespace OpenData.backend
         }
 
         /// <summary>
-        /// Asserts correct status code for all HttpGet calls on the metadatatype controller
+        /// Asserts correct status code and correct content type for all HttpPut calls on the metadatatype controller (includes 1 get test)
         /// </summary>
         [Theory]
         [InlineData("/api/metadatatype")]
-        public async Task Get_EndpointsReturnSuccess(string url)
+        public async Task Put_NewMetadataTypeAndTag_GetByName_EndpointsReturnSuccess_CorrectContentType(string url)
+        {
+            // Arrange
+
+            var client = _factory.CreateDefaultClient();
+            NewUserResource newUserResource = new NewUserResource();
+            newUserResource.Mail = "PutMetTyp@test.kommune.no";
+            newUserResource.Password = "PutMetTyp@passw0rd";
+            newUserResource.UserType = UserType.Municipality;
+            await client.PutAsync("api/user", new StringContent(JsonSerializer.Serialize(newUserResource), Encoding.UTF8, "application/json"));
+
+            AuthUserResource loginResource = new AuthUserResource();
+            loginResource.Mail = newUserResource.Mail;
+            loginResource.Password = newUserResource.Password;
+            var loginResponse = await client.PostAsync("/api/user/auth", new StringContent(JsonSerializer.Serialize(loginResource), Encoding.UTF8, "application/json"));
+
+            Assert.NotNull(loginResponse.Content);
+            var user = ExtractResponse.Extract<PrivateSafeUserResource>(loginResponse);
+            Assert.Equal(loginResource.Mail, user.Mail);
+            string token = user.Token;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            MetadataType resource = new MetadataType();
+            resource.Name = "Test";
+
+            // Act
+
+            var responseForMetadataType = await client.PutAsync(url, new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
+            var responseForTag = await client.PutAsync(url + "/" + resource.Name + "/tag", new StringContent(JsonSerializer.Serialize(resource), Encoding.UTF8, "application/json"));
+            var responseForGetByName = await client.GetAsync("/api/metadatatype/" + resource.Name);
+
+            // Assert
+
+            responseForMetadataType.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                responseForMetadataType.Content.Headers.ContentType.ToString());
+
+            responseForTag.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                responseForTag.Content.Headers.ContentType.ToString());
+
+            responseForGetByName.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                responseForGetByName.Content.Headers.ContentType.ToString());
+        }
+
+        /// <summary>
+        /// Asserts correct status code and correct content type for all HttpGet calls on the metadatatype controller
+        /// </summary>
+        [Theory]
+        [InlineData("/api/metadatatype")]
+        public async Task Get_EndpointsReturnSuccess_CorrectContentType(string url)
         {
             // Arrange
 
@@ -32,28 +89,8 @@ namespace OpenData.backend
             // Assert
 
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-        }
-
-        /// <summary>
-        /// Asserts correct content type for all HttpGet calls on the metadatatype controller
-        /// </summary>
-        [Theory]
-        [InlineData("/api/metadatatype")]
-        public async Task Get_CorrectContentType(string url)
-        {
-            // Arrange
-
-            var client = _factory.CreateDefaultClient();
-
-            // Act
-
-            var response = await client.GetAsync(url);
-
-            // Assert
-
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
-
     }
 }
